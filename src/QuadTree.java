15d12d16b0021351d1e398f2a188288e92cf1a38
Jakob Coker <cokerjako@ecs.vuw.ac.nz>
2020-03-19 23:28:08 +1300

    Updated data files, completed all code and removed known bugs. Created QuadTree and Rectangle classes Did some refactoring.

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -3,7 +3,65 @@
  * @author Jakob Coker
  */
 public class QuadTree {
+    private Rectangle boundary;
+    private int capacity;
+    private QuadTree topLeft;
+    private QuadTree topRight;
+    private QuadTree bottomLeft;
+    private QuadTree bottomRight;
+
+    public QuadTree(Rectangle boundary, int capacity){
+        this.setBoundary(boundary);
+        this.capacity = capacity;
+
+    }
+
+    public void subdivide(){
+    }
 
 
+    public Rectangle getBoundary() {
+        return boundary;
+    }
 
+    public void setBoundary(Rectangle boundary) {
+        this.boundary = boundary;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+
+    public QuadTree getTopLeft() {
+        return topLeft;
+    }
+
+    public void setTopLeft(QuadTree topLeft) {
+        this.topLeft = topLeft;
+    }
+
+    public QuadTree getTopRight() {
+        return topRight;
+    }
+
+    public void setTopRight(QuadTree topRight) {
+        this.topRight = topRight;
+    }
+
+    public QuadTree getBottomLeft() {
+        return bottomLeft;
+    }
+
+    public void setBottomLeft(QuadTree bottomLeft) {
+        this.bottomLeft = bottomLeft;
+    }
+
+    public QuadTree getBottomRight() {
+        return bottomRight;
+    }
+
+    public void setBottomRight(QuadTree bottomRight) {
+        this.bottomRight = bottomRight;
+    }
 }
